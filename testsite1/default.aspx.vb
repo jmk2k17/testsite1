36c2dea7b86023757ff91546eb5d58105fd8850e
jerome mc kenna <jeromef50@aol.com>
2017-06-12 23:34:00 +0100

    fix1

---
+++ testsite1/default.aspx.vb
@@ -0,0 +1,13 @@
+﻿Public Class _default
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim mystring As String
+
+
+    End Sub
+
+    Private Sub _default_PreInit(sender As Object, e As EventArgs) Handles Me.PreInit
+
+    End Sub
+End Class
